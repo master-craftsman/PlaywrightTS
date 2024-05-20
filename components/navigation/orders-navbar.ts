615835a9f96be808805c8ba9e0eb45ab28ec48b7
abeokash <abeokash@rolf.ru>
2024-05-20 09:26:40 +0500

    fix checking count of position

--- components/navigation/orders-navbar.ts
+++ components/navigation/orders-navbar.ts
@@ -2,6 +2,7 @@ import { Page } from '@playwright/test';
 import { Button } from '../../page-factory/button';
 import {ListItem} from "../../page-factory/list-item";
 import {Input} from "../../page-factory/input";
+import {Title} from "../../page-factory/title";
 
 export class OrdersNavbar {
   readonly orderNavbar: OrdersNavbar;
@@ -9,13 +10,13 @@ export class OrdersNavbar {
   private readonly newOrder: Button;
   private readonly positionsSwitcher: Button;
   private readonly orderList: ListItem;
-  private readonly positionsCounter: Input;
+  private readonly positionsCounter: Title;
 
   constructor(public page: Page) {
 
     this.newOrder = new Button({ page, locator: '//button[@type=\'button\' and span[text()=\'New Order\']]', name: 'New order' });
     this.positionsSwitcher = new Button({ page, locator: '//div[text()=\'Positions\']', name: 'Positions switcher' });
-    this.positionsCounter = new Input({ page, locator: '//div[@class="root_hp root_bz root_sz root_kq root_ta root_ee root_ef root_eg root_eh root_cg root_cs root_cp root_co"]', name: 'Positions counter' });
+    this.positionsCounter = new Title({ page, locator: '//div[contains(text(), \'Positions\')]', name: 'Positions counter' });
     this.orderList = new ListItem({ page, locator: '//div[@class="root_cf root_ch root_co root_ci root_cp root_eh root_eg root_bw root_a root_b root_c root_d"]', name: 'Order list item' });
   }
 
@@ -26,7 +27,8 @@ export class OrdersNavbar {
 
   async orderListHasOpenedOrder(expectedCount: number): Promise<void> {
     await this.positionsSwitcher.click();
-    await this.positionsCounter.shouldHaveText(expectedCount.toString());
+    await this.positionsCounter.shouldBeVisible();
+    await this.positionsCounter.shouldHaveText('Positions' + expectedCount.toString());
     // await this.orderList.shouldBeVisible();
     // await this.orderList.shouldHaveCount({}, expectedCount);
   }
